1ebdd154000aa697640580e0b54ac1ceac39210c
sathish <ichbinnp@gmail.com>
2022-06-17 22:28:51 +0530

    vendor and warehouse models and migrations created

---
+++ app/Models/WarehouseOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WarehouseOrder extends Model
+{
+    use HasFactory;
+}
